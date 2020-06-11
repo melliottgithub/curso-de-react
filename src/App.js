08251ea223f6772972d91afb4fbe397e229fd0bb
Michael <kllmmc23@gmail.com>
2020-06-11 15:38:24 -0600

    fragemnts and event clicks

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import Paragraph from './components/Paragraph';
 import Variables from './components/Variables';
+import Events from './components/Events';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hi React</h1>
-        <Paragraph />
-        <Variables />
+        <Events />
       </header>
     </div>
   );
---
+++ src/components/Events.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react'
+
+const Events = () => {
+
+    const handleClick = () => {
+        
+    }
+
+    return (
+        <Fragment>
+            <hr />
+            <h2>Eventos</h2>
+            <button onClick={handleClick}>Click</button>
+        </Fragment>
+    )
+}
+
+export default Events
