5a76965bd5822554e4604845051ccef51800397e
Michael <kllmmc23@gmail.com>
2020-06-11 16:47:49 -0600

    objects arrays

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -1,15 +1,19 @@
 import React, { Fragment, useState } from "react";
 
 const Lists = () => {
-  const initialList = [1, 2, 3, 4, 5];
+  const initialList = [
+    { id: 1, text: "task1" },
+    { id: 2, text: "task2" },
+    { id: 3, text: "task3" },
+  ];
 
   const [list, setList] = useState(initialList);
 
   return (
     <Fragment>
       <h2>Lists</h2>
-      {list.map((item,index) => (
-        <h4 key={index}>{item}</h4>
+      {list.map(({id,text}) => (
+        <h4 key={id}>{text}</h4>
       ))}
     </Fragment>
   );
