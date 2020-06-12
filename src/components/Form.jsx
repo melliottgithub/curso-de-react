c8d39341a2fbf6cc0b8a8082fc42858a0ecf29d6
Michael <kllmmc23@gmail.com>
2020-06-11 18:14:32 -0600

    practiceAll

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,21 +2,33 @@ import React, { Fragment, useState } from "react";
 
 const Form = () => {
   const [fruit, setFruit] = useState("");
-    const [description, setDescription] = useState("");
-    
-    const saveData = (e) => {
-        e.preventDefault()
+  const [description, setDescription] = useState("");
+  const [list, setList] = useState([]);
 
-        if (!fruit.trim()) {
-            return
-        }
-        if (!description.trim()) {
-            return
-        }
-        e.target.reset()
-        setFruit('')
-        setDescription('')
+  const saveData = (e) => {
+    e.preventDefault();
+
+    if (!fruit.trim()) {
+      return;
     }
+    if (!description.trim()) {
+      return;
+    }
+
+    setList([...list, { fruit, description }]);
+
+    e.target.reset();
+    setFruit("");
+    setDescription("");
+  };
+
+  //   const handleChangeFruit = (e) => {
+  //     setFruit(e.target.value);
+  //   };
+
+  //   const handleChangeDescription = (e) => {
+  //     setDescription(e.target.value);
+  //   };
 
   return (
     <Fragment>
@@ -38,8 +50,17 @@ const Form = () => {
           className="form-control mb-2"
           onChange={(e) => setDescription(e.target.value)}
         />
-        <button type='submit' className="btn btn-primary btn-block">Add</button>
+        <button type="submit" className="btn btn-primary btn-block">
+          Add
+        </button>
       </form>
+      <div>
+        {list.map((item, index) => (
+          <p key={index}>
+            {item.fruit} - {item.description}
+          </p>
+        ))}
+      </div>
     </Fragment>
   );
 };
